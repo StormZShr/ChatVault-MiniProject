44d415c9bc06ec0c35998f376aadd596b4ece3b1
Sohrab Rizvi <sohrab.june17@gmail.com>
2026-04-26 01:22:28 +0530

    Removed unnecesarry files for project

--- backend/test_db.py
+++
@@ -1,7 +0,0 @@
-from database import engine
-
-try:
-    with engine.connect() as conn:
-        print("Database Connected successfully")
-except Exception as e:
-    print(f"Connection Failed {e}")
