2925e0c8c4831ce47942578a58ad9ae32661d280
Sohrab Rizvi <sohrab.june17@gmail.com>
2026-05-04 23:34:28 +0530

    Fixed local project Middleware Connection

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="ChatVault API")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://localhost:5173","https://chat-vault-mini-project.vercel.app"],
+    allow_origins=["http://localhost:5173","https://chat-vault-mini-project.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials = True
